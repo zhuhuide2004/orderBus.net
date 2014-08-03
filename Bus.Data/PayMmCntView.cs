109b80124566a3e4d820b1ff0fcf977c2c255683
zhuhuide2004 <zhuhuide2004@163.com>
2014-08-03 13:53:58 +0800

    线路收费

--- Bus.Data/PayMmCntView.cs
+++ Bus.Data/PayMmCntView.cs
@@ -12,56 +12,56 @@ namespace Bus.Data
     {
         public BusEntities entity = new BusEntities();
 
-        //#region Get
-        //public static PayMmCntView GETPayMmCntView(IQueryBuilder<PayMmCntView> iquery)
-        //{
-        //    using (var entity = new BusEntities())
-        //    {
-        //        return entity.PayMmCntView.Where(iquery.Expression).FirstOrDefault();
-        //    }
-        //}
-        //public static PayMmCntView GETPayMmCntView(int ID)
-        //{
-        //    using (var entity = new BusEntities())
-        //    {
-        //        return entity.PayMmCntView.Where(x => x.ID == ID).FirstOrDefault();
-        //    }
-        //}
-        //#endregion
+        #region Get
+        public static PayMmCntView GETPayMmCntView(IQueryBuilder<PayMmCntView> iquery)
+        {
+            using (var entity = new BusEntities())
+            {
+                return entity.PayMmCntView.Where(iquery.Expression).FirstOrDefault();
+            }
+        }
+        public static PayMmCntView GETPayMmCntView(int ID)
+        {
+            using (var entity = new BusEntities())
+            {
+                return entity.PayMmCntView.Where(x => x.ID == ID).FirstOrDefault();
+            }
+        }
+        #endregion
 
-        //#region List
-        //public static List<PayMmCntView> PayMmCntViewList(IQueryBuilder<PayMmCntView> iquery, int Page = 1, int PageSize = 10)
-        //{
-        //    using (var entity = new BusEntities())
-        //    {
-        //        return entity.PayMmCntView.Where(iquery.Expression).OrderByDescending(x => x.ID).Skip(((Page == 0 ? 1 : Page) - 1) * PageSize).Take(PageSize).ToList<PayMmCntView>();
-        //    }
-        //}
+        #region List
+        public static List<PayMmCntView> PayMmCntViewList(IQueryBuilder<PayMmCntView> iquery, int Page = 1, int PageSize = 10)
+        {
+            using (var entity = new BusEntities())
+            {
+                return entity.PayMmCntView.Where(iquery.Expression).OrderByDescending(x => x.ID).Skip(((Page == 0 ? 1 : Page) - 1) * PageSize).Take(PageSize).ToList<PayMmCntView>();
+            }
+        }
 
-        //public static List<PayMmCntView> PayMmCntViewList(IQueryBuilder<PayMmCntView> iquery)
-        //{
-        //    using (var entity = new BusEntities())
-        //    {
-        //        return entity.PayMmCntView.Where(iquery.Expression).OrderByDescending(x => x.ID).ToList();
-        //    }
-        //}
+        public static List<PayMmCntView> PayMmCntViewList(IQueryBuilder<PayMmCntView> iquery)
+        {
+            using (var entity = new BusEntities())
+            {
+                return entity.PayMmCntView.Where(iquery.Expression).OrderByDescending(x => x.ID).ToList();
+            }
+        }
 
-        //public static List<PayMmCntView> PayMmCntViewList()
-        //{
-        //    using (var entity = new BusEntities())
-        //    {
-        //        return entity.PayMmCntView.OrderByDescending(x => x.ID).ToList();
-        //    }
-        //}
-        //public static PagedList<PayMmCntView> List(IQueryBuilder<PayMmCntView> iquery, int Page = 1, int PageSize = 10)
-        //{
-        //    using (var entity = new BusEntities())
-        //    {
-        //        return entity.PayMmCntView.Where(iquery.Expression).OrderByDescending(x => x.ID).ToPagedList(Page, PageSize);
-        //    }
-        //}
+        public static List<PayMmCntView> PayMmCntViewList()
+        {
+            using (var entity = new BusEntities())
+            {
+                return entity.PayMmCntView.OrderByDescending(x => x.ID).ToList();
+            }
+        }
+        public static PagedList<PayMmCntView> List(IQueryBuilder<PayMmCntView> iquery, int Page = 1, int PageSize = 10)
+        {
+            using (var entity = new BusEntities())
+            {
+                return entity.PayMmCntView.Where(iquery.Expression).OrderByDescending(x => x.ID).ToPagedList(Page, PageSize);
+            }
+        }
 
-        //#endregion
+        #endregion
 
     }
 }
