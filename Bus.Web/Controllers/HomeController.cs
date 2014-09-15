0beabc1b8ee71c0f07658add43b272f08266eea2
zhuhuide2004 <zhuhuide2004@163.com>
2014-09-15 10:00:55 +0800

    不得已  只能合并了

--- Bus.Web/Controllers/HomeController.cs
+++ Bus.Web/Controllers/HomeController.cs
@@ -24,6 +24,7 @@ namespace Bus.Web.Controllers
     }
     public class HomeController : BaseController
     {
+
         //
         // GET: /Home/
         //[UserLogin] 
@@ -109,6 +110,17 @@ namespace Bus.Web.Controllers
             }
             return result;
         }
+        //public static bool IsMobileDevice()
+        //{
+        //    string u = Request.ServerVariables["HTTP_USER_AGENT"];
+        //    Regex b = new Regex(@"android.+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino|ucweb|mqqbrowser", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        //    Regex v = new Regex(@"1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-", RegexOptions.IgnoreCase | RegexOptions.Multiline);
+        //    if (b.IsMatch(u))
+        //    {
+        //        return true;
+        //    }
+        //    return v.IsMatch(u.Substring(0, 4));
+        //}
         [UserLogin]
         public ActionResult MyProfile(int ID = 0)
         {
@@ -333,9 +345,31 @@ namespace Bus.Web.Controllers
         {
             return View();
         }
+        public ActionResult MLogin()
+        {
+            return View();
+        }
 
         [HttpPost]
         public ActionResult Login(FormCollection fc)
+        {
+            var phone = fc["Phone"];
+            var p = Encrypt.DES.Des_Encrypt(fc["Password"]);
+            var q = QueryBuilder.Create<Data.Users>().Equals(x => x.Password, p).Equals(x => x.Phone, phone);
+            var model = Data.UsersDB.GETUsers(q);
+            if (model == null)
+            {
+                return Json(new { success = false }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                SetLocal(fc, model.ID);
+                Cookie.WriteCookie("UserID", model.ID.ToString());
+                return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        [HttpPost]
+        public ActionResult MLogin(FormCollection fc)
         {
             var phone = fc["Phone"];
             var p = Encrypt.DES.Des_Encrypt(fc["Password"]);
@@ -402,6 +436,11 @@ namespace Bus.Web.Controllers
         {
             return View();
         }
+        public ActionResult SignOut()
+        {
+            Cookie.DelCookie("UserID");
+            return Content("<script>window.location.href='/Home/';</script>");
+        }
         [HttpPost]
         public ActionResult Reg(FormCollection fc)
         {
@@ -438,7 +477,7 @@ namespace Bus.Web.Controllers
                 if (userID > 0)
                 {
                     SetLocal(fc, userID);
-                    Cookie.WriteCookie("UserID", userID.ToString());
+                    Cookie.WriteCookie("UserID", model.Phone);
                     return Json(new { success = true }, JsonRequestBehavior.AllowGet);
                 }
                 else
@@ -455,7 +494,23 @@ namespace Bus.Web.Controllers
         {
             return View();
         }
+        public ActionResult MRegister(int WxID = 0)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult RegisterFast(FormCollection fc)
+        {
+            var model = new Data.Users();
+            model.Address = fc["hidSAddress"];
+            model.EndAddress = fc["hidEAddress"];
 
+            model.StartLat = TypeConverter.StrToDouble(fc["hidSLat"], 0);
+            model.StartLong = TypeConverter.StrToDouble(fc["hidSLong"], 0);
+            model.EndLat = TypeConverter.StrToDouble(fc["hidELat"], 0);
+            model.EndLong = TypeConverter.StrToDouble(fc["hidELong"], 0);
+            return View(model);
+        }
 
         [HttpPost]
         public ActionResult Register(FormCollection fc)
@@ -465,20 +520,38 @@ namespace Bus.Web.Controllers
                 var model = new Data.Users();
                 model.Address = "";
                 model.CreateTime = DateTime.Now;
-                model.EndLat = 0;
-                model.EndLong = 0;
-                model.EndTime = DateTime.Now;
+                model.EndLat = TypeConverter.StrToDouble(fc["hidELat"], 0);
+                model.EndLong = TypeConverter.StrToDouble(fc["hidELong"], 0);
+                model.StartLat = TypeConverter.StrToDouble(fc["hidSLat"], 0);
+                model.StartLong = TypeConverter.StrToDouble(fc["hidSLong"], 0); 
+                model.Address = fc["beginAddress"];
+                model.EndAddress = fc["EndAddress"];
+
+                //model.EndTime = DateTime.Now;
+                //model.Names = fc["Names"];
+                //model.Password = Encrypt.DES.Des_Encrypt(fc["Password"]);
+                //model.Phone = fc["Phone"];
+                
+                //model.StartTime = DateTime.Now;
+                //model.WXUserID = iRequest.GetQueryInt("WxID", 0);
+                //model.isFinal = true;
+                //model.ParentUserID = 0;
+                //model.Sex = 1;
+                //model.EndAddress = "";
+
+                model.Sex = TypeConverter.StrToInt(fc["Sex"], 1);
+                model.StartTime = TypeConverter.StrToDateTime(fc["StartTime"]);
+                model.EndTime = TypeConverter.StrToDateTime(fc["EndTime"]);
                 model.Names = fc["Names"];
                 model.Password = Encrypt.DES.Des_Encrypt(fc["Password"]);
                 model.Phone = fc["Phone"];
-                model.StartLat = 0;
-                model.StartLong = 0;
-                model.StartTime = DateTime.Now;
+
+
                 model.WXUserID = iRequest.GetQueryInt("WxID", 0);
                 model.isFinal = true;
                 model.ParentUserID = 0;
-                model.Sex = 1;
-                model.EndAddress = "";
+
+
                 model.StateID = 0;
                 model.Number = GetNumber();
                 var q = QueryBuilder.Create<Data.Users>().Equals(x => x.Phone, model.Phone);
@@ -511,6 +584,55 @@ namespace Bus.Web.Controllers
             //    return Json(new { success = false, message = ee.Message }, JsonRequestBehavior.AllowGet);
             //}
         }
+
+        [HttpPost]
+        public ActionResult MRegister(FormCollection fc)
+        {
+            //try
+            //{
+            var model = new Data.Users();
+            model.Address = "";
+            model.CreateTime = DateTime.Now;
+            model.EndLat = TypeConverter.StrToDouble(fc["hidELat"], 0);
+            model.EndLong = TypeConverter.StrToDouble(fc["hidELong"], 0);
+            model.StartLat = TypeConverter.StrToDouble(fc["hidSLat"], 0);
+            model.StartLong = TypeConverter.StrToDouble(fc["hidSLong"], 0);
+            model.Address = fc["beginAddress"];
+            model.EndAddress = fc["EndAddress"];
+
+            model.Sex = TypeConverter.StrToInt(fc["Sex"], 1);
+            model.StartTime = TypeConverter.StrToDateTime(fc["StartTime"]);
+            model.EndTime = TypeConverter.StrToDateTime(fc["EndTime"]);
+            model.Names = fc["Names"];
+            model.Password = Encrypt.DES.Des_Encrypt(fc["Password"]);
+            model.Phone = fc["Phone"];
+
+
+            model.WXUserID = iRequest.GetQueryInt("WxID", 0);
+            model.isFinal = true;
+            model.ParentUserID = 0;
+
+
+            model.StateID = 0;
+            model.Number = GetNumber();
+            var q = QueryBuilder.Create<Data.Users>().Equals(x => x.Phone, model.Phone);
+            var _m = Data.UsersDB.GETUsers(q);
+            if (_m != null)
+            {
+                return Json(new { success = false, message = "该手机号已经注册." }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                if (SendCodes(model.Phone))
+                {
+                    return Json(new { success = true }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "发送手机短信验证码失效." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
         #endregion
 
         #region 查询线路
@@ -531,7 +653,7 @@ namespace Bus.Web.Controllers
             var allBusLine = Data.BusLineDB.BusLineList(q);
             return View(allBusLine);
         }
-        [UserLogin]
+        //[UserLogin]
         public ActionResult GetBusLine(int TID=1)
         {
             var allq = QueryBuilder.Create<Data.BusLine>().Equals(x => x.TypeID, TID);
